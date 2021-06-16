7f6aef196292519dbfd67542fcc2bced5938e81a
Rubi Bhandari <rubiibhandarii@gmail.com>
2021-06-16 21:37:35 +0545

    Added entry setup

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const morgan = require('morgan')
+const chalk = require('chalk')
+const cors = require('cors')
+
+const app = express()
+require('dotenv').config()
+
+// Middleware
+app.use(express.json())
+app.use(cors())
+app.use(morgan('tiny'))
+
+// 404 not found
+app.use((req, res) =>
+    res.status(404).json({
+        success: false,
+        message: `404 Not found: /api/v2${req.url} does not exist`,
+    })
+)
+
+// Error handling
+app.use((err, req, res) =>
+    res.status(err.status || 500).json({
+        success: false,
+        message: err.message,
+    })
+)
+
+// Defining PORT
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () =>
+    console.log(
+        `${chalk.green('✓')} ${chalk.blue(
+            `Listening on http://localhost:${PORT}/.`
+        )}`
+    )
+)
