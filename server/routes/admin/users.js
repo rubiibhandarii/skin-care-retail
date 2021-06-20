872dd2e54925a659e790c10845110a649dfffa75
Rubi Bhandari <rubiibhandarii@gmail.com>
2021-06-20 22:14:31 +0545

    added admin routes

---
+++ server/routes/admin/users.js
@@ -0,0 +1,21 @@
+const router = require('express').Router()
+const { verifyToken, isAdmin } = require('../../middleware/authentication')
+const {
+    all,
+    single,
+    create,
+    update,
+    remove,
+} = require('../../controllers/admin/users')
+
+router.get('/', verifyToken, isAdmin, all)
+
+router.get('/:userId', isAdmin, single)
+
+router.post('/new', verifyToken, isAdmin, create)
+
+router.put('/update/:userId', verifyToken, isAdmin, update)
+
+router.delete('/delete/:userId', verifyToken, isAdmin, remove)
+
+module.exports = router
