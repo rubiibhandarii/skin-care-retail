b69036713421ef090923b863b3c96b979257b739
Rubi Bhandari <rubiibhandarii@gmail.com>
2021-06-20 22:06:48 +0545

    Retailer admin controllers

--- server/controllers/admin/retailers.js
+++ server/controllers/admin/retailers.js
@@ -1,15 +1,15 @@
 const bcrypt = require('bcryptjs')
-const { Provider } = require('../../models')
-const { createValidation } = require('../../validation/admin/providers')
+const { Retailer } = require('../../models')
+const { createValidation } = require('../../validation/admin/retailers')
 
 exports.all = async (req, res, next) => {
     try {
-        const providers = await Provider.findAll()
+        const retailers = await Retailer.findAll()
         return res.status(200).json({
             success: true,
-            message: 'All the available providers are fetched.',
-            count: providers.length,
-            data: providers,
+            message: 'All the available retailers are fetched.',
+            count: retailers.length,
+            data: retailers,
         })
     } catch (err) {
         return next(err)
@@ -17,21 +17,21 @@ exports.all = async (req, res, next) => {
 }
 
 exports.single = async (req, res, next) => {
-    const { providerId } = req.params
+    const { retailerId } = req.params
 
     try {
-        const singleProvider = await Provider.findByPk(providerId)
+        const singleRetailer = await Retailer.findByPk(retailerId)
 
-        if (!singleProvider)
+        if (!singleRetailer)
             return res.status(404).json({
                 success: false,
-                message: 'Provider not found!',
+                message: 'Retailer not found!',
             })
 
         return res.status(200).json({
             success: true,
-            message: 'Single provider is fetched.',
-            data: singleProvider,
+            message: 'Single retailer is fetched.',
+            data: singleRetailer,
         })
     } catch (err) {
         return next(err)
@@ -39,7 +39,7 @@ exports.single = async (req, res, next) => {
 }
 
 exports.create = async (req, res, next) => {
-    const { username, email, password } = req.body
+    const { companyName, location, email, password } = req.body
 
     // Validation
     const { error } = createValidation(req.body)
@@ -50,17 +50,8 @@ exports.create = async (req, res, next) => {
         })
 
     try {
-        // Checking if username exists
-        const usernameExists = await Provider.findOne({ where: { username } })
-
-        if (usernameExists)
-            return res.status(400).json({
-                success: false,
-                message: 'Username was already taken.',
-            })
-
         // Checking if email exists
-        const emailExists = await Provider.findOne({ where: { email } })
+        const emailExists = await Retailer.findOne({ where: { email } })
 
         if (emailExists)
             return res.status(400).json({
@@ -72,16 +63,17 @@ exports.create = async (req, res, next) => {
         const salt = await bcrypt.genSalt(10)
         const hashedPassword = await bcrypt.hash(password, salt)
 
-        const createdProvider = await Provider.create({
-            username,
+        const createdRetailer = await Retailer.create({
+            companyName,
+            location,
             email,
             password: hashedPassword,
             isVerified: true,
         })
         return res.status(200).json({
             success: true,
-            message: 'New provider was added.',
-            data: createdProvider,
+            message: 'New retailer was added.',
+            data: createdRetailer,
         })
     } catch (err) {
         return next(err)
@@ -89,26 +81,26 @@ exports.create = async (req, res, next) => {
 }
 
 exports.update = async (req, res, next) => {
-    const { providerId } = req.params
-    const { username, email, password } = req.body
+    const { retailerId } = req.params
+    const { companyName, location, email, password } = req.body
 
     try {
-        const singleProvider = await Provider.findByPk(providerId)
+        const singleRetailer = await Retailer.findByPk(retailerId)
 
-        if (!singleProvider)
+        if (!singleRetailer)
             return res.status(404).json({
                 success: false,
-                message: 'Provider not found!',
+                message: 'Retailer not found!',
             })
 
-        const updatedProvider = await Provider.update(
-            { username, email, password },
-            { where: { id: providerId } }
+        const updatedRetailer = await Retailer.update(
+            { companyName, location, email, password },
+            { where: { id: retailerId } }
         )
         return res.status(200).json({
             success: true,
-            message: 'Provider was updated.',
-            data: updatedProvider,
+            message: 'Retailer was updated.',
+            data: updatedRetailer,
         })
     } catch (err) {
         return next(err)
@@ -116,24 +108,24 @@ exports.update = async (req, res, next) => {
 }
 
 exports.remove = async (req, res, next) => {
-    const { providerId } = req.params
+    const { retailerId } = req.params
 
     try {
-        const singleProvider = await Provider.findByPk(providerId)
+        const singleRetailer = await Retailer.findByPk(retailerId)
 
-        if (!singleProvider)
+        if (!singleRetailer)
             return res.status(404).json({
                 success: false,
-                message: 'Provider not found!',
+                message: 'Retailer not found!',
             })
 
-        const deletedProvider = await Provider.destroy({
-            where: { id: providerId },
+        const deletedRetailer = await Retailer.destroy({
+            where: { id: retailerId },
         })
         return res.status(200).json({
             success: true,
-            message: 'Provider was deleted.',
-            data: deletedProvider,
+            message: 'Retailer was deleted.',
+            data: deletedRetailer,
         })
     } catch (err) {
         return next(err)
