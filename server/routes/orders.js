59c5c4b558996cc4649b77ba94c7746707befeab
Rubi Bhandari <rubiibhandarii@gmail.com>
2021-06-20 21:44:25 +0545

    updated orders

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const { verifyToken, isUser } = require('../middleware/authentication')
-const { create } = require('../controllers/categories')
+const { create } = require('../controllers/orders')
 
 router.post('/new', verifyToken, isUser, create)
 
